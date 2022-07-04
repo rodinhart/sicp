10f56347bf8b64a16b4500a98b6dda2a0fd6c8ca
Rodin van der Hart <rodin.hart@orgvue.com>
2022-07-04 08:45:30 +0100

    lambda

--- repl.js
+++ repl.js
@@ -12,6 +12,10 @@ class Cons {
   }
 
   *[Symbol.iterator]() {
+    if (this.x === undefined) {
+      return
+    }
+
     yield this.x
     let c = this.y
     while (!isNil(c)) {
@@ -21,13 +25,21 @@ class Cons {
   }
 }
 
-const nil = null
+const nil = new Cons()
 const cons = (x, y) => new Cons(x, y)
 const car = (p) => p.x
 const cdr = (p) => p.y
 const setCdr = (p, y) => (p.y = y)
 const isPair = (p) => p instanceof Cons
 const isNil = (p) => p === nil
+const list = (...xs) => {
+  let r = nil
+  for (let i = xs.length - 1; i >= 0; i -= 1) {
+    r = cons(xs[i], r)
+  }
+
+  return r
+}
 
 const read = (s) => {
   const _ = (xs) => {
@@ -39,7 +51,9 @@ const read = (s) => {
         setCdr(c, cons(_(xs), nil))
         c = cdr(c)
       }
-      xs.shift()
+      if (xs.shift() !== ")") {
+        throw new Error("Missing )")
+      }
 
       return cdr(r)
     }
@@ -55,25 +69,48 @@ const read = (s) => {
   )
 }
 
-const eval = (exp, env) => {
+// need env with parent
+const evaluate = (exp, locals) => {
   if (typeof exp === "number") {
-    return exp
+    return String(exp)
   }
 
   if (typeof exp === "symbol") {
     const key = Symbol.keyFor(exp)
-    if (!(key in env)) {
-      // need env with parent
-      throw new Error(`Unknown symbol ${key}`)
-    }
 
-    return env[key]
+    return locals.has(key) ? key : `getVar("${key}")`
   }
 
   if (isPair(exp)) {
-    const [op, ...args] = [...exp].map((x) => eval(x, env))
+    const arr = [...exp]
+    const prim = typeof arr[0] === "symbol" ? Symbol.keyFor(arr[0]) : undefined
+    if (prim === "define") {
+      if (isPair(arr[1])) {
+        return evaluate(
+          list(
+            arr[0],
+            car(arr[1]),
+            list(Symbol.for("lambda"), cdr(arr[1]), arr[2])
+          ),
+          locals
+        )
+      }
 
-    return op(...args)
+      const name = Symbol.keyFor(arr[1])
+      const val = evaluate(arr[2], locals)
+
+      return `(setVar("${name}", ${val}),Symbol.for("${name}"))`
+    } else if (prim === "lambda") {
+      const params = [...arr[1]].map((x) => Symbol.keyFor(x))
+      const newLocals = new Set([...locals, ...params])
+      const body = evaluate(arr[2], newLocals)
+
+      return `((${params.join(",")})=>${body})`
+    }
+
+    const [op, ...args] = arr.map((x) => evaluate(x, locals))
+
+    return `${op}(${args.join(",")})`
   }
 
   return exp
@@ -100,7 +137,7 @@ const prn = (exp) => {
   }
 
   if (typeof exp === "function") {
-    return `[PROC ${exp.name}]`
+    return `[PROC ${exp.name || "LAMBDA"}]`
   }
 
   return exp
@@ -108,6 +145,21 @@ const prn = (exp) => {
 
 const core = {
   "+": (...ns) => ns.reduce((r, n) => r + n, 0),
+  "-": (n, ...ns) => (ns.length === 0 ? -n : ns.reduce((r, n) => r - n, n)),
+  "/": (n, ...ns) => ns.reduce((r, n) => r / n, n),
+  "*": (...ns) => ns.reduce((r, n) => r * n, 1),
+}
+
+const getVar = (key) => {
+  if (!(key in core)) {
+    throw new Error(`Unknown symbol ${key}`)
+  }
+
+  return core[key]
+}
+
+const setVar = (key, val) => {
+  core[key] = val
 }
 
 const _ = () => {
@@ -116,10 +168,16 @@ const _ = () => {
       rl.close()
       // console.log("Bye bye")
       return
+    } else if (answer === "") {
+      _()
+
+      return
     }
 
     try {
-      console.log(prn(eval(read(answer), core)))
+      const compiled = evaluate(read(answer), new Set())
+      // console.log(compiled)
+      console.log(prn(eval(compiled)))
     } catch (e) {
       console.error(e)
     }
